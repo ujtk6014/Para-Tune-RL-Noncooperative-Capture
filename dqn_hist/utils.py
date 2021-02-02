8d929c4d76c84ad86f80cf5d896d39b8d7e69ba6
Yuji Tanaka <57528969+ujtk6014@users.noreply.github.com>
2021-02-02 12:23:08 +0900

    UPDATE: don&t allow negative param

--- dqn_hist/utils.py
+++ dqn_hist/utils.py
@@ -58,8 +58,6 @@ def mini_batch_train(env, agent, max_episodes, max_steps, batch_size):
 def mini_batch_train_adaptive(env, agent, max_episodes, max_steps, batch_size, time_window):
     episode_rewards = []
     counter = 0
-    k_delta = 0.01
-    D_delta = 1e-5
     state_num = 7 #姿勢角４・角速度３
     try:
         with tqdm(range(max_episodes),leave=False) as pbar:
@@ -83,19 +81,31 @@ def mini_batch_train_adaptive(env, agent, max_episodes, max_steps, batch_size, t
                         n= str(Base_10_to_n(action,3))
                         while len(n) < 4:
                             n ="0"+n
-                        para = np.empty((4,1))
+                        sign = [0]*4
                         for i in range(4):
                             if n[i] == '0':
-                                para[i] = 1
+                                sign[i] = 1
                             elif n[i] == '1':
-                                para[i] = 0
+                                sign[i] = 0
                             elif n[i] == '2':
-                                para[i] = -1
-                            k += k_delta * para[0]
-                            D[0,0] += D_delta * para[1]
-                            D[4,4] += D_delta * para[2]
-                            D[8,8] += D_delta * para[3]
-                            para = np.empty((4,1))
+                                sign[i] = -1
+                            para = [k,D[0,0],D[4,4],D[8,8]]
+                            para_exp = [np.log10(para[i]) for i in range(len(para))]
+                            para_exp_f = [np.floor(para_exp[i]) for i in range(len(para))]
+                            delta_tmp = [0]*len(para)
+                            delta_tmp[0] = para_exp_f[0]-1 if para_exp[0].is_integer() else para_exp_f[0]
+                            delta_tmp[0] = -2.0 if delta_tmp[0] >= -2 else delta_tmp[0]
+                            delta_tmp[1:] = [para_exp_f[i+1]-1 if para_exp[i+1].is_integer() else para_exp_f[i+1] for i in range(len(para)-1)]
+                            delta_tmp[1:] = [-5.0 if delta_tmp[i+1] >=-5  else delta_tmp[i+1] for i in range(len(para)-1)]
+
+                            delta = [10**(delta_tmp[i]) for i in range(len(delta_tmp))]
+                            for i in range(len(para)):
+                                para[i] += delta[i]*sign[i]
+                            k = para[0]
+                            D[0,0] = para[1]
+                            D[4,4] = para[2]
+                            D[8,8] = para[3]
+                            sign = [0]*4
                             if k<0 or D[0,0]<0 or D[4,4]<0 or D[8,8] <0:
                                 env.neg_param_flag = False
                     W = obs[4:7]
